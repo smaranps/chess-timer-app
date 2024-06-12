f86278bf0fb1d1c64b84e08ae105936e1f2d352f
smaranps <smaranpinisetty@gmail.com>
2024-06-12 16:57:29 -0400

    Added timer for player 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,12 @@ import { SafeAreaView } from "react-native";
 export default function HomeScreen() {
   const [user1Time, setuser1Time] = useState<number>(180);
   const [user1TimeRunning, setuser1TimeRunning] = useState<Boolean>(false);
+  const [user2TimeRunning, setuser2TimeRunning] = useState<Boolean>(false);
   const [user2Time, setuser2Time] = useState<number>(180);
+  const [user2TimeInterval, setUser2TimeInterval] =
+    useState<NodeJS.Timeout | null>(null);
+  const [user1TimeInterval, setUser1TimeInterval] =
+    useState<NodeJS.Timeout | null>(null);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -21,10 +26,15 @@ export default function HomeScreen() {
             if (user1TimeRunning) {
               return;
             }
+            setuser2TimeRunning(false);
             setuser1TimeRunning(true);
-            setInterval(() => {
+            if (user2TimeInterval !== null) {
+              clearInterval(user2TimeInterval);
+            }
+            let user1ClockIntervalVar = setInterval(() => {
               setuser1Time((user1Time) => user1Time - 1);
             }, 1000);
+            setUser1TimeInterval(user1ClockIntervalVar);
           }}
         >
           <Text style={{ fontSize: 70, textAlign: "center" }}>
@@ -33,7 +43,29 @@ export default function HomeScreen() {
         </TouchableOpacity>
       </View>
       <View style={{ flex: 1, backgroundColor: "lightblue" }}></View>
-      <View style={{ flex: 5, backgroundColor: "yellow" }}></View>
+      <View style={{ flex: 5, backgroundColor: "yellow" }}>
+        <TouchableOpacity
+          onPress={() => {
+            if (user2TimeRunning) {
+              return;
+            }
+            setuser1TimeRunning(false);
+            setuser2TimeRunning(true);
+            if (user1TimeInterval !== null) {
+              clearInterval(user1TimeInterval);
+            }
+
+            let user2ClockIntervalVar = setInterval(() => {
+              setuser2Time((user2Time) => user2Time - 1);
+            }, 1000);
+            setUser2TimeInterval(user2ClockIntervalVar);
+          }}
+        >
+          <Text style={{ fontSize: 70, textAlign: "center" }}>
+            {Math.floor(user2Time / 60)}:{user2Time % 60}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
